24fb8dfcd251bec32376a0d67e7086d9fa4f5070
mrdannael <mr.dannael@gmail.com>
2017-03-24 00:58:16 +0100

    fix beep boop context import

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const translate = require('google-translate-api')
 const Slapp = require('slapp')
 const ConvoStore = require('slapp-convo-beepboop')
-const Context = require('context')
+const Context = require('slapp-context-beepboop')
 
 var slapp = Slapp({
     convo_store: ConvoStore(),
